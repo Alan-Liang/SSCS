97fccec2c8c8d10ecc01314760242c876f919c25
Alan-Liang <16379418+Alan-Liang@users.noreply.github.com>
2018-06-12 22:01:52 +0800

    dev commit: transfer to ws

--- server.js
+++ server.js
@@ -9,7 +9,6 @@
 var fs=require("fs");
 var mime=require("mime");
 var url=require("url");
-var vurl=require("./vurl");
 var template=require("./template");
 var ws=require("ws");
 
@@ -28,7 +27,7 @@ var handleRequest=sscs.prototype.handleRequest=function(req,resp){
 		if(type=="text/html")
 			type="text/html;charset=utf-8";
 		resp.writeHead(200, {'Content-Type':type});
-		var file=(type=="text/html;charset=utf-8")?this.processHtml(cache[pathname]):cache[pathname];	
+		var file=this.processStatic(cache[pathname]);
 		resp.write(file);
 		resp.end();
 		return;
@@ -36,25 +35,27 @@ var handleRequest=sscs.prototype.handleRequest=function(req,resp){
 	if(pathname==""){
 		var type="text/html";
 		resp.writeHead(200, {'Content-Type':type});	
-		resp.write(this.processHtml(cache["index.html"]));
+		resp.write(this.processStatic(cache["index.html"]));
 		resp.end();
 		return;
 	}
-	if(vurl.query(pathname)){
-		try{
-			vurl.query(pathname).call(this,req,resp);
+	if(pathname=="login"){		
+		var trc;
+		if((trc=this.pendReq(req))!=false){
+			var params = url.parse(req.url,true).query;
+			resp.writeHead(302, {'Location':'/chat.html?rc='+encodeURI(trc)+'&uname='+encodeURI(params.uname)});
+			resp.end();
 			return;
-		}catch(e){
-			try{
-				clog("Error executing "+pathname+" : "+e.stack);
-				resp.writeHead(501, {'Content-Type':'text/plain'});	
-				resp.end();
-				return;
-			}catch(e){}
 		}
+		resp.writeHead(302, {'Location':'/'});
+		resp.write(this.errmsg.roomNotExist);
+		resp.end();
+		return;
 	}
+	resp.writeHead(404);
+	resp.end();
 };
-sscs.prototype.processHtml=function(str){
+sscs.prototype.processStatic=function(str){
 	return template(str.toString(),this);
 };
 sscs.prototype.title="SSCS";
@@ -65,13 +66,18 @@ sscs.prototype.logoutUrl="/";
 sscs.prototype.sendText="Send";
 sscs.prototype.sendLabel="Message...";
 sscs.prototype.timeJustNow="Just Now";
+sscs.prototype.error="Error";
+sscs.prototype.messageSent="Message Sent.";
+sscs.prototype.wsLocation="\"+location.origin.replace(\"https://\",\"\").replace(\"http://\",\"\")+\"/";
+sscs.prototype.networkFault="Something went wrong with the network! Please tell the admin if you are sure your network is good, or click <a href=\\\"javascript:void(0);\\\" onclick=\\\"location.reload();\\\">here</a> to reload.";
+sscs.prototype.reconnecting="Reconnecting...";
 
 sscs.prototype.endl="\r\n";
 sscs.prototype.errmsg={
 	roomNotExist:"This room does not exist.",
 	internal:"Something bad happens. See log for details.",
 	jsonInvalid:"The webpage encounters an error. For more details, the JSON it send is invalid.",
-	badRequest:"The webpage sent us a bad request."
+	badRequest:"The webpage sent us a bad request.",
 };
 sscs.prototype.wsConnections={};
 var handleWs=sscs.prototype.handleWs=function(wsc,req){
@@ -87,7 +93,7 @@ var handleWs=sscs.prototype.handleWs=function(wsc,req){
 		wscs=wscs[trc];
 		wsc._dataPile="";
 		var endl=this.endl;
-		wsc.send("HIST"+this.getHistory(trc)+endl);
+		wsc.send("HIST "+encodeURIComponent(JSON.stringify(this.getHistory(trc)))+endl);
 		wsc.addEventListener('message',function(msg){
 			wsc._dataPile+=msg.data;
 			var cmds=wsc._dataPile.split(endl);
@@ -100,18 +106,18 @@ var handleWs=sscs.prototype.handleWs=function(wsc,req){
 							break;
 						var params;
 						try{
-							params=JSON.parse(args[1]);
+							params=JSON.parse(decodeURIComponent(args[1]));
 						}catch(e){
 							wsc.send("ERR! "+this.errmsg.jsonInvalid+endl);
 						}
 						if(params["time"]&&params["user"]&&params["text"]){
 							var obj={
-								time:decodeURI(params.time),
-								user:decodeURI(params.user),
-								text:decodeURI(params.text)
+								time:params.time,
+								user:params.user,
+								text:params.text
 							};
 							this.pushHistory(trc,obj);
-							var msg=JSON.stringify(obj);
+							var msg=encodeURIComponent(JSON.stringify(obj));
 							for(var i=0;i<wscs.length;i++){
 								var c=wscs[i];
 								try{
@@ -119,13 +125,14 @@ var handleWs=sscs.prototype.handleWs=function(wsc,req){
 								}catch(e){}
 							}
 						}
+						break;
 						
 						case "ERR!":
 						clog("client complaints our fault: "+args[1]);
 						break;
 						
 						default:
-						wsc.send("ERR!"+this.errmsg.badRequest+endl);
+						wsc.send("ERR! "+this.errmsg.badRequest+endl);
 						break;
 					}
 				}
@@ -220,7 +227,7 @@ sscs.prototype.stopsvc=function(){
 	}
 };
 
-sscs.prototype.pages=["mdc.css","mdc.js","chat.html","index.html"];
+sscs.prototype.pages=["mdc.css","mdc.js","chat.html","index.html","sscs.css","sscs.js"];
 sscs.prototype.loadPages=function(){
 	for(var i=0;i<this.pages.length;i++){
 		try{
@@ -238,89 +245,3 @@ sscs.prototype.pendReq=function(req){
 		if(params["rc"]==this.rc[i])return this.rc[i];
 	return false;
 }
-
-//add listening functions
-vurl.add={path:"webapi/history",func:function(req,resp){
-	var trc;
-	if((trc=this.pendReq(req))!=false){
-		resp.writeHead(200, {'Content-Type':'application/json'});
-		resp.write(JSON.stringify({history:this.getHistory(trc)}));
-		resp.end();
-		return;
-	}
-	resp.writeHead(403, {'Content-Type':'text/plain'});
-	resp.write("403 unauthorized");
-	resp.end();
-}};
-vurl.add={path:"webapi/new",func:function(req,resp){
-	var trc;
-	if((trc=this.pendReq(req))!=false){
-		var params = url.parse(req.url,true).query;
-		if(params["last"]){
-			resp.writeHead(200, {'Content-Type':'application/json'});
-			var news={news:this.getNews(trc,parseInt(params.last))};
-			resp.write(JSON.stringify(news));
-			resp.end();
-			return;
-		}
-		resp.writeHead(400, {'Content-Type':'application/json'});
-		resp.write(JSON.stringify({err:400,message:"Bad parameter"}));
-		resp.end();
-		return;
-	}
-	resp.writeHead(403, {'Content-Type':'application/json'});
-	resp.write(JSON.stringify({err:403,message:"Unauthorized"}));
-	resp.end();
-}};
-vurl.add={path:"webapi/add",func:function(req,resp){
-	var trc;
-	if((trc=this.pendReq(req))!=false){
-		var postData="";
-		req.setEncoding("utf8");
-		req.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-		});
-		req.addListener("end", function() {
-			try{
-				var params = JSON.parse(postData);
-				if(params["time"]&&params["user"]&&params["text"]){
-					var id=this.pushHistory(trc,{
-						time:decodeURI(params.time),
-						user:decodeURI(params.user),
-						text:decodeURI(params.text)
-					});
-					resp.writeHead(200, {'Content-Type':'application/json'});
-					resp.write("{status:"+id+"}");
-					resp.write(postData);
-				}
-				else{
-					resp.writeHead(200, {'Content-Type':'application/json'});
-					resp.write("{status:-1}");
-				}
-				resp.end();
-			}catch(e){
-				resp.writeHead(501, {'Content-Type':'application/json'});
-				clog("Error adding message :"+e.stack);
-				resp.end(JSON.stringify({err:501,message:"Error adding message."}));
-			}
-		}.bind(this));
-	}else{
-		resp.writeHead(403, {'Content-Type':'application/json'});
-		resp.write(JSON.stringify({err:403,message:"Unauthorized"}));
-		resp.end();
-	}
-}};
-
-vurl.add={path:"webapi/login",func:function(req,resp){
-	var trc;
-	console.log(req);
-	if((trc=this.pendReq(req))!=false){
-		var params = url.parse(req.url,true).query;
-		resp.writeHead(302, {'Location':'/chat.html?rc='+encodeURI(trc)+'&uname='+encodeURI(params.uname)});
-		resp.end();
-		return;
-	}
-	resp.writeHead(302, {'Location':'/'});
-	resp.write("Unauthorized");
-	resp.end();
-}};
---
+++ sscs.js
@@ -0,0 +1,229 @@
+var sscs=function(options){
+
+var templ=options.messageTemplate;
+var loginErrorTemplate=options.loginErrorTemplate;
+var chatroom=options.chatroom;
+var msgbox=options.messageBox;
+var sendbtn=options.sendButton;
+var snackbarbox=options.snackbar;
+var dialogbox=options.dialog;
+var dialogTitle=options.dialogTitle;
+var dialogDesc=options.dialogDescription;
+
+
+//time formatting
+var timeago=function(time){
+	if(time=="")return "";
+	if(typeof time==typeof "")
+		time=new Date(time);
+	var now=new Date();
+	var diff=now.getTime()-time.getTime();
+	if(diff<10*1000) //10 sec
+		return "{{timeJustNow}}";
+	if(diff>365*24*60*60*1000) //1 year
+		return time.getFullYear()+"/"+(time.getMonth()+1)+"/"+time.getDate();
+	if(diff>30*24*60*60*1000) //1 month
+		return (time.getMonth()+1)+"/"+time.getDate();
+	if(diff>24*60*60*1000){ //1 day
+		var dayAgo=parseInt(diff/(24*60*60*1000));
+		var s=(dayAgo==1)?"":"s";
+		return dayAgo+" day"+s+" ago";
+	}
+	if(diff>60*60*1000){ //1h
+		var hrAgo=parseInt(diff/(60*60*1000));
+		var s=(hrAgo==1)?"":"s";
+		return hrAgo+" hour"+s+" ago";
+	}
+	if(diff>60*1000){ //1m
+		var minAgo=parseInt(diff/(60*1000));
+		var s=(minAgo==1)?"":"s";
+		return minAgo+" minute"+s+" ago";
+	}
+	var secAgo=parseInt(diff/1000);
+	var s=(secAgo==1)?"":"s";
+	return secAgo+" second"+s+" ago";
+};
+
+function debug(str){
+	var debug=true;
+	if(debug)console.log("[SSCS-DEBUG] "+str);
+}
+function snackbar(content,action,callback,timeout){
+	if(action===undefined)
+		action="OK";
+	var obj;
+	if(typeof content==typeof {})
+		obj=content;
+	else obj={
+		message:content,
+		actionText:action,
+		timeout:timeout||5000,
+		actionHandler:callback||function(){}
+	};
+	snackbarbox.MDCSnackbar.show(obj);
+}
+sscs.snackbar=snackbar;
+
+var _alert=alert;
+alert=function(title,desc){
+	dialogTitle.innerHTML=title;
+	dialogDesc.innerHTML=desc||"";
+	dialogbox.MDCDialog.show();
+}
+
+var rc;
+try{
+	var a=(window.location.search.substr(/rc=/.exec(window.location.search).index+3));
+	var b=/&/.exec(a);
+	b=b?b.index:undefined;
+	a=a.substr(0,b);
+	rc=a;
+}catch(e){}
+var user;
+try{
+	var a=(window.location.search.substr(/uname=/.exec(window.location.search).index+6));
+	var b=/&/.exec(a);
+	b=b?b.index:undefined;
+	a=a.substr(0,b);
+	user=a;
+}catch(e){}
+var last=-1;
+function tpl(chatmsg,str,idn){
+	var st=/{{[a-zA-Z0-9]*}}/gi;
+	var res="";
+	var comp;
+	var last=0;
+	var flag=false;
+	while(comp=st.exec(str)){
+		res+=str.substring(last,comp.index);
+		flag=true;
+		last=comp.index+comp[0].length;
+		var pps=str.substring(comp.index+2,last-2);
+		var val;
+		if(pps=="text" && (chatmsg[idn][pps].indexOf("@"+user)>-1))
+			chatmsg[idn][pps]="<font class=\"atme\">"+chatmsg[idn][pps]+"</font>";
+/*		if(pps=="text"&&/\[Code:.*\]/.test(chatmsg[idn][pps])){
+			/\[Code:.*\]/gi.exec(chatmsg[idn][pps]).index;
+		}*/
+		if((val=chatmsg[idn][pps])!=undefined)res+=val;
+		else res+=("{{"+pps+"}}");
+	}
+	res+=str.substring(last,str.length);
+	return res;
+}
+var newdiv;
+function loadmsg(chatmsg,clearflag){
+	if(clearflag)chatroom.innerHTML="";
+	for(var i=0;i<chatmsg.length;i++){
+		newdiv=document.createElement("div");
+		newdiv.className="message-wrapper";
+		chatmsg[i].timeago=timeago(chatmsg[i].time);
+		newdiv.innerHTML=tpl(chatmsg,templ,i);
+		chatroom.appendChild(newdiv);
+		if(("#msg_"+chatmsg[i].id)==location.hash){
+			var off=newdiv.offsetTop;
+			setTimeout(function(){
+				window.scrollTo(0,off);
+			},10);
+			newdiv.firstElementChild.classList.add("sscs-focused");
+			(function(fc){
+				setTimeout(function(){
+					fc.classList.add("sscs-focused-blur");
+					fc.classList.remove("sscs-focused");
+				},500);
+			})(newdiv.firstElementChild);
+		}
+	}
+	debug(templ);
+	var a=chatmsg[chatmsg.length-1];
+	var b=a?a:{id:-1};
+	return b.id;
+}
+//websockets
+var wsLoc=(/https/.test(location.href)?"wss://":"ws://")+"{{wsLocation}}?rc="+rc;
+var ws = new WebSocket(wsLoc);
+var maxTries=7;
+var closeListener=function(e){
+	if(e.code!=1000){
+		debug("Retrying."+maxTries);
+		if(maxTries<=0){
+			alert("{{networkFault}}");
+			return;
+		}
+		maxTries--;
+		ws=new WebSocket(wsLoc);
+		ws.addEventListener('close',closeListener);
+		ws.onopen=wsOpen;
+		snackbar("{{reconnecting}}"," ",null,1e64);
+	}
+};
+ws.addEventListener('close',closeListener);
+
+var endl="\r\n";
+ws.onopen = wsOpen = function(event) {
+  debug("WebSocket opened.");
+  maxTries=7;
+  ws._dataPile="";
+  ws.onmessage = function(msg) {
+		ws._dataPile+=msg.data;
+		var cmds=ws._dataPile.split(endl);
+		if(cmds.length>1){
+			for(var i=0;i<cmds.length-1;i++){
+				debug(cmds);
+				debug(i);
+				var args=cmds[i].split(" ");
+				switch(args[0]){
+					case "ERR!":
+					alert("{{error}}",args[1]);
+					break;
+
+					case "CHAT":
+					try{
+						var res=JSON.parse(decodeURIComponent(args[1]));
+						loadmsg([res]);
+						//todo
+					}catch(e){
+						alert("{{error}}",e.stack);
+					}
+					break;
+					
+					case "HIST":
+					try{
+						var res=JSON.parse(decodeURIComponent(args[1]));
+						loadmsg(res,true);
+					}catch(e){
+						alert("{{error}}",e.stack);
+					}
+					break;
+
+					default: //unknown
+					ws.send("ERR!"+endl);
+					break;
+				}
+			}
+			ws._dataPile=cmds[cmds.length-1];
+		}
+  };
+};
+
+sendbtn.onclick=function(){
+	if(!(msgbox.value&&(msgbox.value!="")))return;
+	var obj={"time":new Date(),
+		"user":user,
+		"text":msgbox.value
+	};
+	ws.send("CHAT "+encodeURIComponent(JSON.stringify(obj))+endl);
+	snackbar("{{messageSent}}","OK",function(){
+		msgbox.focus();
+	});
+	msgbox.value="";
+};
+
+msgbox.onkeypress=function(e){
+	if(e.charCode==13)sendbtn.onclick();
+};
+
+};
+sscs.showTime=function(time){
+	alert(new Date(time).toString());
+};
